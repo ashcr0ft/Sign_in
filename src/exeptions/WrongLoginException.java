7a4176efc608afd9835d21770e8a1256279535f5
mlstlit <Litvinov.v@mail.ru>
2024-11-03 16:02:05 +0300

    unchecked exceptions

--- src/exeptions/WrongLoginException.java
+++ src/exeptions/WrongLoginException.java
@@ -1,6 +1,6 @@
 package exeptions;
 
-public class WrongLoginException extends Exception {
+public class WrongLoginException extends RuntimeException {
     public WrongLoginException(String message) {
         super(message);
     }
--- src/exeptions/WrongPasswordException.java
+++ src/exeptions/WrongPasswordException.java
@@ -1,6 +1,6 @@
 package exeptions;
 
-public class WrongPasswordException extends Exception {
+public class WrongPasswordException extends RuntimeException {
     public WrongPasswordException(String message) {
         super(message);
     }
